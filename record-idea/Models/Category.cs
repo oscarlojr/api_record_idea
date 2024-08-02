26d8e3be9547df8a9e702823b74a2150fd3c6b22
Oscar Lima Oliveira Junior <oscarlojr@gmail.com>
2024-08-02 18:16:44 -0300

    feat: Create the CategoriesController

--- record-idea/Models/Category.cs
+++ record-idea/Models/Category.cs
@@ -1,12 +1,13 @@
-namespace record_idea.Models;
-
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
-public class Category {
+namespace record_idea.Models;
+
+public class Category
+{
     [BsonId]
-    [BsonRepresentation(BsonType.ObjectId)]
-    public string Id { get; set; } = null!;
+    [BsonRepresentation(BsonType.String)]
+    public string Id { get; set; } = null!; // Não inicializar aqui
 
     public string Name { get; set; } = null!;
     public string Description { get; set; }
--- record-idea/Program.cs
+++ record-idea/Program.cs
@@ -1,51 +1,10 @@
-// var builder = WebApplication.CreateBuilder(args);
-//
-// // Add services to the container.
-// // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-// builder.Services.AddEndpointsApiExplorer();
-// builder.Services.AddSwaggerGen();
-//
-// var app = builder.Build();
-//
-// // Configure the HTTP request pipeline.
-// if (app.Environment.IsDevelopment())
-// {
-//     app.UseSwagger();
-//     app.UseSwaggerUI();
-// }
-//
-// app.UseHttpsRedirection();
-//
-// var summaries = new[]
-// {
-//     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-// };
-//
-// app.MapGet("/weatherforecast", () =>
-// {
-//     var forecast =  Enumerable.Range(1, 5).Select(index =>
-//         new WeatherForecast
-//         (
-//             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-//             Random.Shared.Next(-20, 55),
-//             summaries[Random.Shared.Next(summaries.Length)]
-//         ))
-//         .ToArray();
-//     return forecast;
-// })
-// .WithName("GetWeatherForecast")
-// .WithOpenApi();
-//
-// app.Run();
-//
-// record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-// {
-//     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-// }
-
 using record_idea.Configurations;
+using record_idea.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 builder.Services.Configure<DatabaseSettings>(
     builder.Configuration.GetSection("DatabaseSettings"));
@@ -56,7 +15,16 @@ builder.Services.AddControllers();
 
 var app = builder.Build();
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
 app.UseHttpsRedirection();
+
 app.UseAuthorization();
+
 app.MapControllers();
+
 app.Run();
