5acee4dea41f16bc0626e3ba88446997cf50178b
Oscar Lima Oliveira Junior <oscarlojr@gmail.com>
2024-08-04 07:54:41 -0300

    refactor: Remove the comments

--- record-idea/Program.cs
+++ record-idea/Program.cs
@@ -4,11 +4,9 @@ using record_idea.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.Configure<DatabaseSettings>(
     builder.Configuration.GetSection("DatabaseSettings"));
 
-// Registro dos repositórios e serviços
 builder.Services.AddSingleton<ICategoryRepository, CategoryRepository>();
 builder.Services.AddSingleton<IIdeaRepository, IdeaRepository>();
 builder.Services.AddSingleton<CategoryService>();
@@ -16,13 +14,11 @@ builder.Services.AddSingleton<IdeaService>();
 
 builder.Services.AddControllers();
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
